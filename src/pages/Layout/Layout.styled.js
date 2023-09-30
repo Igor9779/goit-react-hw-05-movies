d85b4c6ffc9ea64a71a77db65631c521b7ffc527
Igor9779 <darthhoit97@gmail.com>
2023-09-30 15:27:13 +0300

    styled3

--- src/pages/Layout/Layout.styled.js
+++ src/pages/Layout/Layout.styled.js
@@ -21,7 +21,7 @@ export const Nav = styled.nav`
   padding-top: 12px;
   padding-bottom: 12px;
   color: #fff;
-  background-color: red;
+  background-color: black;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
@@ -33,7 +33,7 @@ export const SNavLink = styled(NavLink)`
   margin-right: 30px;
   text-decoration: none;
   &.active {
-    color: #000;
+    color: red;
     font-weight: 700;
     transform: scale(1.1);
   }
--- src/pages/MovieDetails/MovieDetails.styled.js
+++ src/pages/MovieDetails/MovieDetails.styled.js
@@ -22,6 +22,7 @@ export const MovieContainer = styled.div`
   display: flex;
   padding: 30px;
   background-color: red;
+    border-radius: 6px;
   margin: 20px 30px 30px;
   box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
     0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
